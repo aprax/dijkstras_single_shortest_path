83ac951e77b9e5d6bd32256d371665caa3e2f026
Adam Prax <adam.prax@gmail.com>
2022-07-04 11:52:13 -0700

    partial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,49 @@
-import type { ShortestEdgeSoFarMap, Edge } from './types';
-
-// Each val must be unique.
-function findShortestPath(root: string, nodes: Map<string, Edge[]>) {
-	const relaxed = new Set([root]);
-	const shortestEdgeSoFarMap: ShortestEdgeSoFarMap = new Map([[root, [root, 0]]]) // These are the shorest distances that we know so far
-
-	if (!nodes.get(root)?.[0]) {
-		return shortestEdgeSoFarMap;
-	}
-
-	const queue: Edge[] = nodes.get(root)!;
-
-	for (const edge of queue) {
-		const shortestEdgeSoFar = shortestEdgeSoFarMap.get(edge[0])?.[1] ?? NaN;
-		if (edge[1] < shortestEdgeSoFar) {
-			shortestEdgeSoFarMap.set(edge[0], [root, edge[1]]);
-		} else if (!shortestEdgeSoFar) {
-			shortestEdgeSoFarMap.set(edge[0], [root, edge[1]]);
+function shortestDistance(grid: number[][]): number {
+	let min = Number.MAX_SAFE_INTEGER;
+	let x = 0;
+	const distance = new Map();
+	while (x++ < grid.length) {
+		let y = 0;
+		const visited = new Set();
+		Array.defineProperty(Array.prototype, 'enqueue', function (this: any[], value: number) {
+			this.push(value);
 		}
-	}
+		while (y++ < grid?.[x]?.length!) {
+			if (!visited.has([x, y])) {
+				const building = new Map();
+				const initial: [number, number][] = [[x, y]];
+				const queue = Object.assign<[number, number][], { enqueue: Function }>(initial, {
+					enqueue: function (cell: [number, number], d: number) { //TODO(aprax) extend the array prototype for fast_queue with a enqueue and deqeueue instead of creating an entirely new class
+						if (!visited.has(cell)) {
+							const [x, y] = cell;
+							if (grid[x]?.[y] === 0) {
+								distance.set(cell, d);
+							}
+						}
 
-	while (queue.length) {
-		const node = queue.shift();
-		if (!node) {
-			throw new Error('queue is unexpecedly undefined');
-		}
 
-		const edges = (nodes.get(node[0]) ?? []);
-		edges.sort((first, second) => {
-			if (first[1] > second[1]) {
-				return 1;
-			} else if (first[1] === second[1]) {
-				return 0;
-			} else {
-				return -1;
-			}
-		});
-		for (const edge of edges) {
-			if (shortestEdgeSoFarMap.has(edge[0])) {
-				const nodeTotalDistance = shortestEdgeSoFarMap.get(node[0])![1] + edge[1];
-				const edgeCurrentTotalDistance = shortestEdgeSoFarMap.get(edge[0])![1];
-				if (nodeTotalDistance < edgeCurrentTotalDistance) {
-					shortestEdgeSoFarMap.set(edge[0], [
-						// New Parent
-						node[0],
-						// New shortest distance
-						nodeTotalDistance])
-				}
-			} else { // Distance is infinity
-				shortestEdgeSoFarMap.set(edge[0], [node[0], edge[1] + node[1]]);
-			}
-			if (!relaxed.has(edge[0])) {
-				queue.push(edge);
+
+					}
+				});
+
+				console.log(test[0]);
+
+				test.enqueue([[5]], 5);
+
+				//const test = new Queue();
+				/*
+				const queue: Queue = {...[[x, y]], ...{
+					enqueue: function (arg, distance) {
+						return 5;
+
+					}
+				}} as Queue;
+				*/
+
 			}
 		}
-		// After calculating the shortest distance to each edge, relax the node.
-		relaxed.add(node[0]);
 	}
 
-	return shortestEdgeSoFarMap;
-}
-export default findShortestPath
+};
+
+export default shortestDistance
